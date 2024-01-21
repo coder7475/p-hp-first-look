b18146e4526cb1fc97424272dfe61db56e7842a9
coder7475 <robiulhossain7475@gmail.com>
2024-01-21 15:19:03 +0600

    feat: variable scope

--- scss/README.md
+++
@@ -1,9 +0,0 @@
-# What is Saas?
-- Sass stands for Syntactically Awesome Stylesheet
-- Sass is an extension of CSS
-- Sass is a CSS pre-processor
-- Sass is completely compatiable with all version of CSS
-
-# Why Use Sass?
-- Sass reduces repetition of CSS and therefore saves time
-- Sass lets us use features that do not exist in CSS, like variables, nested rules, mixins, imports, inheritance, built-in functions, and other stuff.
---
+++ variables/variableScope.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scope of Variables</title>
+</head>
+
+<body>
+    <h1>PHP Variables Scope</h1>
+    <section>
+        <h2>Variable Scope</h2>
+        <p>In PHP, variables can be declared anywhere in the script. But the scope of a variable is the part of the
+            script
+            where the variabel can be <strong>referenced/used.</strong>
+        </p>
+        <p>PHP has three different variable scopes:</p>
+        <ul>
+            <li>local</li>
+            <li>global</li>
+            <li>static</li>
+        </ul>
+    </section>
+    <br>
+
+</body>
+
+</html>
