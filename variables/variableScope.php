b8688b97cc7ccf09ff4884b73bcb3c677a1f5c55
coder7475 <robiulhossain7475@gmail.com>
2024-01-21 17:03:45 +0600

    feat: global scope example

--- variables/variableScope.php
+++ variables/variableScope.php
@@ -23,7 +23,19 @@
         </ul>
     </section>
     <br>
+    <section>
+        <?php 
+        $x = 5; // global scope
 
+        function myTest(  ) { 
+            // using x inside this function will generate an error
+            echo "<p>Variable x is inside function is: $x</p>";
+        }
+
+        myTest();
+        echo "<p>Variable x is inside function is: $x</p>";
+        ?>
+    </section>
 </body>
 
 </html>
