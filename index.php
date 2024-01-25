55ef82b6c09b87cd4eb5ae6db9a235b047a2aca7
coder7475 <robiulhossain7475@gmail.com>
2024-01-25 18:24:58 +0600

    learn: PHP string

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
-
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Intro</title>
+    <link rel="stylesheet" href="./../style.css">
+</head>
 <body>
     <h1>My first PHP page</h1>
 
