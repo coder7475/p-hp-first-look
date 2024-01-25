5f8663c9f6bd20514d25392ef434ac5fc7e9925e
coder7475 <robiulhossain7475@gmail.com>
2024-01-25 18:51:21 +0600

    feat: learn uppercasing and lowercasing function

--- strings/modify.php
+++ strings/modify.php
@@ -9,5 +9,21 @@
 <body>
     <h1>PHP - Modify Strings</h1>
     <p>PHP has a set of built in functions to modify strings.</p>
+
+    <section>
+        <h2>Upper Case</h2>
+        <p>The <strong>strtoupper()</strong> function returns the string in upper case:</p>
+        <?php 
+        $x = "Hello World!";
+        echo strtoupper($x);
+        ?>
+        <h2>Lower Case</h2>
+        <p>The <strong>strtolower()</strong> function returns the string in lower case:</p>
+        <?php 
+        $x = "Hello World!";
+        echo strtolower($x);
+        ?>
+    </section>
+
 </body>
 </html>
