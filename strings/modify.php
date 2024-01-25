c5ab82e5f2fdd97af97c31f81614cc6862bbedb8
coder7475 <robiulhossain7475@gmail.com>
2024-01-25 18:45:56 +0600

    feat: create a file to learn in built functions to modify strings

---
+++ strings/modify.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modify Strings</title>
+    <link rel="stylesheet" href="./../style.css">
+</head>
+<body>
+    <h1>PHP - Modify Strings</h1>
+    <p>PHP has a set of built in functions to modify strings.</p>
+</body>
+</html>
